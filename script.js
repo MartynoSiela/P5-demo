f304999fcfa10c4c6aa7ae97c2f5eaf215e08657
MartynoSiela <m.satas@gmail.com>
2026-04-11 11:23:24 +0300

    Refactor p5 logic to support multiple projects

---
+++ script.js
@@ -0,0 +1,10 @@
+function projectSelected() {
+    const projectsSelect = document.getElementById('projects');
+
+    if (projectsSelect.value === 'bouncables') {
+        setupScript = setupBouncables;
+        drawScript = drawBouncables;
+    }
+
+    setupScript();
+}
